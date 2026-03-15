34d2e9d935bf73ce0925de4f8e8847a7ccb581e1
DenissiusMaxximus <deniskramar07@gmail.com>
2026-03-15 23:38:50 +0200

    Add Singleton and Prototype classes with project files

---
+++ Prototype/Program.cs
@@ -0,0 +1,44 @@
+﻿public class Virus
+{
+    public int Weight { get; set; }
+    public int Age { get; set; }
+    public string Name { get; set; }
+    public string Type { get; set; }
+    public List<Virus> Children { get; set; }
+
+    public Virus(int weight, int age, string name, string type)
+    {
+        Weight = weight;
+        Age = age;
+        Name = name;
+        Type = type;
+        Children = new List<Virus>();
+    }
+
+    public Virus Clone()
+    {
+        var clone = new Virus(Weight, Age, Name, Type)
+        {
+            Children = [.. Children.Select(child => child.Clone())]
+        };
+        return clone;
+    }
+}
+
+public static class Program
+{
+    public static void Main()
+    {
+        Virus parent = new(12, 3, "Alpha", "A");
+        parent.Children.Add(new Virus(3, 1, "Alpha-1", "A1"));
+
+        Virus clone = parent.Clone();
+
+        Console.WriteLine("Оригінал:");
+        Console.WriteLine($"Name: {parent.Name}, Child: {parent.Children[0].Name}");
+
+        Console.WriteLine("Клон:");
+        Console.WriteLine($"Name: {clone.Name}, Child: {clone.Children[0].Name}");
+    }
+}
+
